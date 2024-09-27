f7561a87db110771e583d2cc7eccf444fe6c0588
aishwarya prasanth <aiswarya.prasanth@keyvalue.systems>
2024-09-27 13:58:17 +0530

    adding settings page and docking leftbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import ChatPage from "./pages/ChatPage";
 import SettingsPage from "./pages/SettingsPage";
 
+import "./App.css";
+import ChatSidePanel from "./containers/ChatSidePanel";
+
 const App = () => {
+  const location = useLocation();
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-      </Routes>
-    </>
+    <div className="flex h-screen w-full">
+      {location.pathname !== "/" && (
+        <div className="flex flex-col w-20 bg-[#1B2A41] border border-r-gray-1">
+          <ChatSidePanel />
+        </div>
+      )}
+      <div className="w-full">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,14 +1,11 @@
 import React from "react";
-import ChatSidePanel from "../containers/ChatSidePanel";
 import ChatsListPanel from "../containers/ChatsListPanel";
 import ChatsMessagePanel from "../containers/ChatsMessagePanel";
 
 const ChatPage = () => {
   return (
     <div className="flex w-full h-screen">
-      <div className="flex flex-col w-20 bg-[#1B2A41] border border-r-[#2D3838]">
-        <ChatSidePanel />
-      </div>
+      
       <div className="flex flex-col w-3/12 bg-[#1B2A41]">
         <ChatsListPanel />
       </div>

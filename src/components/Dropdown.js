6fc32ab6be0c2acb3695794c9234de1bc3b6cd1a
AbhijitSKurup <abhijit@keyvalue.systems>
2024-09-27 14:26:44 +0530

    tailwind class extend styles, dropdown props added

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,45 +1,20 @@
-import React from "react";
+import React from 'react';
 
-const Dropdown = ({ title, options }) => {
+const Dropdown = ({ selected, options, onSelect }) => {
   return (
-    <>
-      <button
-        id="dropdownDefaultButton"
-        data-dropdown-toggle="dropdown"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-      >
-        {title}
-        <svg
-          class="w-2.5 h-2.5 ms-3"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 10 6"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m1 1 4 4 4-4"
-          />
-        </svg>
-      </button>
-
-      <div
-        id="dropdown"
-        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-      >
-        <ul
-          class="py-2 text-sm text-gray-700 dark:text-gray-200"
-          aria-labelledby="dropdownDefaultButton"
-        >
-          {options?.length &&
-            options?.map((item) => <li key={item?.id}>{item?.value}</li>)}
-        </ul>
-      </div>
-    </>
+    <div className="p-4 bg-primary text-white rounded-md w-64">
+    <select
+      className="w-full p-2 bg-primary text-white border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      value={selected}
+      onChange={(e) => onSelect(e.target.value)}
+    >
+      {options.map((option, index) => (
+        <option key={index} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      primary: "#0C1821",
-      white: "#ffffff",
-      gray: {
-        1: "#2D3838", // borders
-        2: "#A3BFBA", // text
-      },
-      purple: {
-        1: "#BEB4F4",
+    extend: {
+      colors: {
+        primary: "#0C1821",
+        white: "#ffffff",
+        gray: {
+          1: "#2D3838", // borders
+          2: "#A3BFBA", // text
+        },
+        purple: {
+          1: "#BEB4F4",
+        },
       },
     },
-    extend: {},
   },
   plugins: [],
 };

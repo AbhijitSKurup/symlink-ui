04c8fc56a2611cae1c7f9ddb5a3982cbd20baf9b
vaishnav <vaishnav@keyvalue.systems>
2024-09-27 17:11:26 +0530

    socket connection

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,5 +3,7 @@ import { ReactComponent as FolderIcon } from "./folder.svg";
 import { ReactComponent as CompassIcon } from "./compass.svg";
 import { ReactComponent as ChatIcon } from "./message.svg";
 import { ReactComponent as BookmarkIcon } from "./bookmark.svg";
+import { ReactComponent as SendIcon } from "./Button.svg";
+import { ReactComponent as PaperClip } from "./paperclip.svg";
 
-export { SearchIcon , BookmarkIcon, ChatIcon, CompassIcon, FolderIcon};
+export { SearchIcon , BookmarkIcon, ChatIcon, CompassIcon, FolderIcon, SendIcon, PaperClip};
